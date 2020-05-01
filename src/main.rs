3a239797851fe1e716846cb42a13e6040f7f8082
Dimitri Belopopsky <dimitri@belopopsky.com>
2020-05-01 14:58:56 +0200

    Add ray, circle intersection and sky gradient

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,22 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-#[derive(Copy, Clone)]
-struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32,
+mod maths;
+use maths::*;
+
+struct Ray {
+    origin: Vec3,
+    dir: Vec3,
 }
 
-impl Vec3 {
-    fn new(x: f32, y: f32, z: f32) -> Self {
-        Vec3 { x, y, z }
+impl Ray {
+    fn new(origin: Vec3, dir: Vec3) -> Self {
+        Ray { origin, dir }
     }
 
-    fn add(self, vec: Self) -> Self {
-        Vec3 {
-            x: self.x + vec.x,
-            y: self.y + vec.y,
-            z: self.z + vec.z,
-        }
+    fn at(self, t: f32) -> Vec3 {
+        self.origin.add(self.dir.mult_float(t))
     }
-
-    fn sub(self, vec: Self) -> Self {
-        Vec3 {
-            x: self.x - vec.x,
-            y: self.y - vec.y,
-            z: self.z - vec.z,
-        }
-    }
-
-    fn length(self) -> f32 {
-        f32::sqrt(self.length_squared())
-    }
-
-    fn neg(self) -> Self {
-        Vec3 {
-            x: -self.x,
-            y: -self.y,
-            z: -self.z,
-        }
-    }
-
-    fn length_squared(self) -> f32 {
-        self.x * self.x + self.y * self.y + self.z * self.z
-    }
-
-    fn mult(self, vec: Vec3) -> Self {
-        Vec3 {
-            x: self.x * vec.x,
-            y: self.y * vec.y,
-            z: self.z * vec.z,
-        }
-    }
-
-    fn div(self, t: f32) -> Self {
-        self.mult(self.at(1.0 / t))
-    }
-
-    fn at(self, t: f32) -> Self {
-        Vec3 {
-            x: self.x * t,
-            y: self.y * t,
-            z: self.z * t,
-        }
-    }
-
-    fn dot(self, vec:Vec3) -> f32 {
-        self.x * vec.x  + self.y * vec.y + self.z * vec.z
-    }
-
-    fn cross(self, vec:Vec3) -> Self {
-        Vec3{
-            x: self.y * vec.z - self.z * vec.y,
-            y: self.z * vec.x - self.x * vec.z,
-            z: self.x * vec.y - self.y * vec.x
-        }
-    }
-
-    fn unit(self) -> Self {
-        self.div(self.length())
-    }
-}
-
-#[cfg(tests)]
-mod vec3_tests {
-    use super::*;
 }
 
 fn create_ppm(name: &str, pixels: &Vec<u8>, width: u32, height: u32) -> std::io::Result<()> {
@@ -97,25 +29,106 @@ fn create_ppm(name: &str, pixels: &Vec<u8>, width: u32, height: u32) -> std::io:
     Ok(())
 }
 
+#[derive(Copy, Clone)]
+struct SimpleCamera {
+    origin: Vec3,
+    lower_left: Vec3,
+    vertical: Vec3,
+    horizontal: Vec3,
+}
+
+impl SimpleCamera {
+    fn new(origin: Vec3, lower_left: Vec3, vertical: Vec3, horizontal: Vec3) -> Self {
+        SimpleCamera {
+            origin,
+            lower_left,
+            vertical,
+            horizontal,
+        }
+    }
+
+    fn get(self, u: f32, v: f32) -> Vec3 {
+        self.lower_left + self.horizontal * u + self.vertical * v
+    }
+}
+
+fn ray_color(ray: &Ray) -> Vec3 {
+    let cercle = Cercle::new(Vec3::new(0.0, 0.0, -1.0), 0.5);
+
+    if hit_sphere(&cercle, &ray) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
+
+    let unit_vec = ray.dir.unit();
+    let t = 0.5 * (unit_vec.y + 1.0);
+    Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
+}
+
+struct Cercle {
+    position: Vec3,
+    radius: f32,
+}
+
+impl Cercle {
+    fn new(position: Vec3, radius: f32) -> Self {
+        Cercle { position, radius }
+    }
+}
+
+fn hit_sphere(cercle: &Cercle, ray: &Ray) -> bool {
+    let oc = cercle.position - ray.origin;
+    let a = ray.dir.dot(ray.dir);
+    let b = 2.0 * oc.dot(ray.dir);
+    let c = oc.dot(oc) - cercle.radius * cercle.radius;
+
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant > 0.0
+}
+
 fn main() {
     println!("Hello, raytracer!");
 
     let image_width = 200;
-    let image_height = 200;
+    let image_height = 100;
+
+    let camera = SimpleCamera::new(
+        Vec3::new(0.0, 0.0, 0.0),
+        Vec3::new(-2.0, -1.0, -1.0),
+        Vec3::new(0.0, 2.0, 0.0),
+        Vec3::new(4.0, 0.0, 0.0),
+    );
 
     let mut pixels: Vec<f32> = vec![];
 
-    for j in (0..image_height).rev() {
+    for j in 0..image_height {
         for i in 0..image_width {
-            pixels.push(i as f32 / image_width as f32);
-            pixels.push(j as f32 / image_height as f32);
-            pixels.push(0.2);
+            let u: f32 = i as f32 / image_width as f32;
+            let v: f32 = (image_height - 1 - j) as f32 / image_height as f32;
+
+            let ray = Ray::new(camera.origin, camera.get(u, v));
+
+            let color = ray_color(&ray);
+
+            pixels.push(color.x);
+            pixels.push(color.y);
+            pixels.push(color.z);
+
+            // color gradient
+            // pixels.push(u);
+            // pixels.push(v);
+            // pixels.push(0.2);
         }
     }
 
-    let output_pixels = pixels.iter().map(|x| (256f32 * x) as u8).collect();
+    let output_pixels = pixels.iter().map(|x| (255.9 * x) as u8).collect();
 
     println!("Generating image!");
-    let _res = create_ppm("gradient.ppm", &output_pixels, image_width, image_height);
+    let _res = create_ppm(
+        "sky_gradient.ppm",
+        &output_pixels,
+        image_width,
+        image_height,
+    );
     println!("Done!")
 }
---
+++ src/maths/vec3.rs
@@ -0,0 +1,139 @@
+use std::ops::Add;
+use std::ops::Mul;
+use std::ops::Div;
+use std::ops::Sub;
+use std::ops::Neg;
+
+#[derive(Copy, Clone)]
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Vec3 { x, y, z }
+    }
+
+    pub fn add(self, vec: Self) -> Self {
+        Vec3 {
+            x: self.x + vec.x,
+            y: self.y + vec.y,
+            z: self.z + vec.z,
+        }
+    }
+
+    pub fn sub(self, vec: Self) -> Self {
+        Vec3 {
+            x: self.x - vec.x,
+            y: self.y - vec.y,
+            z: self.z - vec.z,
+        }
+    }
+
+    pub fn length(self) -> f32 {
+        f32::sqrt(self.length_squared())
+    }
+
+    pub fn neg(self) -> Self {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+
+    pub fn length_squared(self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn div(self, t: f32) -> Self {
+        self.mult_float(1.0 / t)
+    }
+
+    pub fn mult(self, vec: Vec3) -> Self {
+        Vec3 {
+            x: self.x * vec.x,
+            y: self.y * vec.y,
+            z: self.z * vec.z,
+        }
+    }
+
+    pub fn mult_float(self, t: f32) -> Self {
+        Vec3 {
+            x: self.x * t,
+            y: self.y * t,
+            z: self.z * t,
+        }
+    }
+
+    pub fn dot(self, vec: Vec3) -> f32 {
+        self.x * vec.x + self.y * vec.y + self.z * vec.z
+    }
+
+    pub fn cross(self, vec: Vec3) -> Self {
+        Vec3 {
+            x: self.y * vec.z - self.z * vec.y,
+            y: self.z * vec.x - self.x * vec.z,
+            z: self.x * vec.y - self.y * vec.x,
+        }
+    }
+
+    pub fn unit(self) -> Self {
+        self.div(self.length())
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Vec3 {
+        self.add(other)
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, other: Vec3) -> Vec3 {
+        self.mult(other)
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, other: f32) -> Vec3 {
+        self.mult_float(other)
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Vec3 {
+        self.sub(other)
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, other: f32) -> Vec3 {
+        self.div(other)
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Vec3 {
+        Vec3::new(-self.x, -self.y, -self.z)
+    }
+}
+
+#[cfg(tests)]
+mod vec3_tests {
+    use super::*;
+}
